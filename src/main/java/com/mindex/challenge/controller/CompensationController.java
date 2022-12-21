9982581d700a48060377edeaed7bc419ec9fd944
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2022-12-20 19:59:16 -0500

    Adding Compensation Controller and all previously unversioned files

---
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -0,0 +1,14 @@
+package com.mindex.challenge.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class CompensationController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CompensationController.class);
+
+
+}
